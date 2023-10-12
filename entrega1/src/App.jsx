01aadec389743b56440bd76645e7876e05a396c0
juan cruz benitez arce <juancruzbeniteza@gmail.com>
2023-10-12 20:09:17 -0300

    Version Fianal

--- entrega1/src/App.jsx
+++ entrega1/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './App.css';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button } from "react-bootstrap";
 import MyNavbar from './componentes/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // Images
 import producto1Image from './media/CARGO-PARACHUTE-NEGRO-1b.jpg';
@@ -11,6 +12,7 @@ import producto3Image from './media/CLASSIC-HOODIE-OVERSIZED-BEIGE-1b.jpg';
 import producto4Image from './media/College-Polo-Shirt-Beige-1b.jpg';
 import producto5Image from './media/SWEATPANT-VERDE-1.jpg';
 import producto6Image from './media/XPD-BLANCO-1.jpg';
+
 
 function App() {
   const Elegancia = "Elegancia";
@@ -51,21 +53,26 @@ function App() {
   const [selectedProduct, setSelectedProduct] = useState(null); // Track the selected product
   const [shoppingCart, setShoppingCart] = useState([]); // Initialize the shopping cart as an empty array
 
+
   const openModal = (product) => {
     setSelectedProduct(product);
     setShowModal(true);
   };
 
+
   const addToCart = (product) => {
     setShoppingCart([...shoppingCart, product]); // Add the selected product to the shopping cart
   };
+
 
   return (
     <div className="app-container">
       <MyNavbar storeName={Elegancia} categories={categories} />
 
+
       <div className="container content-container">
         <h1 className="title">Bienvenido a {Elegancia}</h1>
+
 
         <h2 className="subtitle">Categorías</h2>
         <ul>
@@ -75,6 +82,7 @@ function App() {
             </li>
           ))}
         </ul>
+
 
         <h2 className="subtitle">Productos Destacados</h2>
         <div className="row">
@@ -102,6 +110,7 @@ function App() {
         </div>
       </div>
 
+
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>{selectedProduct ? selectedProduct.name : ''}</Modal.Title>
@@ -127,5 +136,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
